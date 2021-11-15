a080cd9693faf6bde855686a92ab5189d8583dcd
Yogeshwaran <yogeshpandian27@gmail.com>
2021-11-15 10:03:05 +0000

    2nd commit

--- src/app.js
+++ src/app.js
@@ -19,14 +19,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Yogesh'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Yogesh'
     })
 })
 
@@ -34,7 +34,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Yogesh'
     })
 })
 
@@ -80,7 +80,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Yogesh',
         errorMessage: 'Help article not found.'
     })
 })
